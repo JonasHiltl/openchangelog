ea68f5730052dd2ed76c14b5b28719b162fab955
Sergio <77530549+sergi0g@users.noreply.github.com>
2024-09-07 22:10:41 +0300

    feat: dark mode #5
    Add dark mode + fix logo centering bug

--- components/style_templ.go
+++ components/style_templ.go
@@ -27,7 +27,7 @@ func DefaultStyle() templ.Component {
 			templ_7745c5c3_Var1 = templ.NopComponent
 		}
 		ctx = templ.ClearChildren(ctx)
-		_, templ_7745c5c3_Err = templ_7745c5c3_Buffer.WriteString("<div class=\"mx-auto w-full flex-1 flex flex-col justify-between prose prose-sm sm:prose-base prose-p:my-2 prose-p:leading-6 prose-h1:mb-3 prose-h2:mb-2 prose-h3:mb-2 prose-a:no-underline hover:prose-a:underline prose-a:underline-offset-4 prose-a:text-primary hover:prose-a:text-primary-highlight prose-img:rounded\">")
+		_, templ_7745c5c3_Err = templ_7745c5c3_Buffer.WriteString("<div class=\"mx-auto w-full flex-1 flex flex-col justify-between prose prose-sm sm:prose-base prose-p:my-2 prose-p:leading-6 prose-h1:mb-3 prose-h2:mb-2 prose-h3:mb-2 prose-a:no-underline hover:prose-a:underline prose-a:underline-offset-4 prose-a:text-primary hover:prose-a:text-primary-highlight prose-img:rounded dark:prose-invert\">")
 		if templ_7745c5c3_Err != nil {
 			return templ_7745c5c3_Err
 		}
